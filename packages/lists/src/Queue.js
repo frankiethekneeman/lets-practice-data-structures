863657f81247555aae39cd9630adca8a8dc7e505
Greg Lopez <42146599+glopez0909@users.noreply.github.com>
2020-08-10 12:42:13 -0400

    Update Queues

--- packages/lists/src/Queue.js
+++ packages/lists/src/Queue.js
@@ -6,8 +6,12 @@
 class Queue {
   /**
    *  Queue Constructors take no arguments
+   *  Queues have a head and a tail 
    */
   constructor() {
+    this._head = null; // this is the object or item
+    this._tail = null; // this points to the next object or not
+    // think of these as one single "being" with a head and a tail
   }
   
   /****************
@@ -22,6 +26,25 @@ class Queue {
    *  @spaceComplexity O(1)
    */
   enqueue(item) {
+    if (!item) {
+      throw `${item} is not valid`;
+    }
+
+    const newNode = {
+      item: item,
+      points: null,
+    };
+
+   if (this.isEmpty()) {
+      this._head = newNode
+      this._tail = this._head;
+    } else { //this I don't fully gronk
+      this._tail.points = newNode;
+      // does this reassign the previous newNode's point to = newNode?
+      this._tail = this._tail.points;
+      // and then this assigns this._tail to be the newNode?
+    };
+    
   }
 
   /**
@@ -33,6 +56,21 @@ class Queue {
    *  @spaceComplexity O(1)
    */
   dequeue() {
+    if (this.isEmpty()){
+      throw `Nothing to remove! The queue is empty.`;
+    }
+    const itemToReturn = this._head.item; 
+    //this allows us to return the item on top
+    this._head = this._head.points;
+    //this reassigns this._head to either be a newNode or empty
+    if (this._head === null) {
+      this._tail = null;
+      //this resets the queue to be an empty queue
+      //can't have a head be null and a tail point to something - both have to
+      //be null or have newNodes
+    }
+    
+    return itemToReturn;
   }
 
   /*****************
@@ -48,6 +86,12 @@ class Queue {
    *  @spaceComplexity O(1)
    */
   peek() {
+    if (this.isEmpty()) {
+      throw `Nothing to peek at! The queue is empty.`;
+    } else {
+      return this._head.item;
+      //this just returns the first object's item
+    }
   }
 
   /**
@@ -58,6 +102,7 @@ class Queue {
    *  @spaceComplexity O(1)
    */
   isEmpty() {
+    return (this._head === null);
   }
 }
 
